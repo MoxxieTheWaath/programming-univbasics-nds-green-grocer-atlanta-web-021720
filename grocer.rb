f9d17b6150a7e1f6f4854481d3a9cd777686e299
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 00:30:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,9 +18,8 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   index = 0
   bop = []
-  print cart
   while index < cart.length do
-    
+    print "#{cart[index]} \n"
     index += 1
   end
   print index
