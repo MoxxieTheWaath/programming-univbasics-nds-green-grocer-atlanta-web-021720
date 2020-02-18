454ff28c7f6f9ef6beff7b1fb5e0bb7c73bca373
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 00:24:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,10 +18,6 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   i = 0
   bop = {}
-  while i < cart.length do
-    bop[i][:item] = "mehp"
-    i += 1
-  end
 end
 
 def apply_coupons(cart, coupons)
